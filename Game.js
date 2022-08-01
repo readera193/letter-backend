639cf30ec6559b8e65542d120706f0e2466a1766
Tim195 <readera193@gmail.com>
2022-08-01 14:46:49 +0800

    refactor: data structure sending

--- Game.js
+++ Game.js
@@ -7,56 +7,71 @@ const fisherYatesShuffle = (arr) => {
 }
 
 const Game = module.exports = {
-    state: "join",
+    state: "waiting",
     usedCards: [0, 0, 0, 0, 0, 0, 0, 0, 0],
     cardPool: [1, 1, 1, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 7, 8],
-    playerNames: ["Reader", "Juliet"],
-    // playerNames: [],
+    actionQueue: [],
     actionPlayer: "",
-    shields: ["Reader"],
-    playerCards: {},
+    playerState: {
+        "Irena": { card: 0, shield: false, gameover: false, action: false },
+        // "Juliet": { card: 0, shield: false, gameover: false, action: false },
+        // "Reader": { card: 0, shield: false, gameover: false, action: false },
+    },
 
     join(playerName) {
-        if (Game.playerNames.length >= 4) {
+        let playerNames = Object.keys(Game.playerState);
+        if (Game.state !== "waiting") {
+            return { status: 423, msg: "遊戲已開始，請稍等" };
+        } else if (playerNames.length >= 4) {
             return { status: 423, msg: "房間已滿" };
-        } else if (Game.playerNames.includes(playerName)) {
+        } else if (playerNames.includes(playerName)) {
             return { status: 401, msg: "暱稱已被使用，請輸入其他暱稱" };
         } else {
-            Game.playerNames.push(playerName);
+            Game.playerState[playerName] = { card: 0, shield: false, gameover: false, action: false };
             return { status: 200, msg: "成功" };
         }
     },
 
     start() {
+        let playerNames = Object.keys(Game.playerState);
+        if (playerNames.length < 2) {
+            throw "需要 2 ~ 4 位玩家";
+        }
         Game.usedCards = [0, 0, 0, 0, 0, 0, 0, 0, 0];
         Game.cardPool = fisherYatesShuffle([1, 1, 1, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 7, 8]);
         Game.playerCards = {};
+        Game.actionQueue = playerNames;
 
-        Game.cardPool.splice(0, Game.playerNames.length).forEach((card, index) => {
-            Game.playerCards[Game.playerNames[index]] = card;
+        playerNames.forEach((playerName) => {
+            Game.playerState[playerName] = Game.cardPool.shift();
         });
-        // Game.actionPlayer = Game.playerNames.shift();
     },
 
     deal() {
+        Game.actionPlayer = Game.actionQueue.shift();
         return Game.cardPool.shift();
     },
 
-    gameState() {
-        return JSON.stringify({
-            usedCards: Game.usedCards,
-            shields: Game.shields,
-            actionPlayer: Game.actionPlayer,
-        });
+    updateData() {
+        let data = JSON.parse(JSON.stringify(Game));
+
+        for (let key of Object.keys(data.playerState)) {
+            delete data.playerState[key].card;
+        }
+        delete data.cardPool;
+        data.type = "update";
+
+        return JSON.stringify(data);
     },
 
     action() {
 
     },
 
-    exit(playerName) {
-        Game.playerNames = Game.playerNames.filter((name) => name !== playerName);
-        Game.shields = Game.shields.filter((name) => name !== playerName);
-        // next player if someone exit at its round
+    gameover(playerName) {
+        console.log("gameover", playerName);
+        Game.playerState[playerName].gameover = true;
+        Game.actionQueue = Game.actionQueue.filter((name) => name !== playerName);
+        // next player if someone exit at his round
     },
 }
--- websocket.js
+++ websocket.js
@@ -5,36 +5,50 @@ const Game = require("./Game");
 module.exports = (server) => {
     const wsServer = new Server({ server });
 
+    const update = () => {
+        let data = Game.updateData();
+        wsServer.clients.forEach((client) => {
+            client.send(data);
+        });
+    };
+
     wsServer.on("connection", (ws) => {
         ws.on("message", (data) => {
-            let { action, playerName } = JSON.parse(data);
-            if (action === "join") {
-                console.log(playerName, "joined game", Game.playerNames);
-                ws.playerName = playerName;
+            try {
+                let { action, playerName } = JSON.parse(data);
+
+                console.log(new Date().toLocaleString("zh-TW", { "hour12": false }),
+                    ws.playerName || playerName, "send", JSON.parse(data));
+
+                if (action === "join") {
+                    console.log(playerName, "joined game");
+                    ws.playerName = playerName;
+                    update();
+                } else if (action === "start") {
+                    Game.start();
+                    let card = Game.deal();
+                    update();
+                    console.log(Game, card);
+                } else if ([1, 2, 3, 4, 5, 6, 7, 8].includes(action)) {
+                } else {
+                    console.log("unknown action:", action);
+                }
+            } catch (error) {
+                console.log("error:", error);
                 wsServer.clients.forEach((client) => {
+                    console.log("client:", client.playerName);
                     client.send(JSON.stringify({
-                        state: "join",
-                        playerNames: Game.playerNames,
-                        // shields: Game.shields,
+                        type: "error",
+                        msg: error,
                     }));
                 });
-            } else if (action === "start") {
-            } else if (action === "getCard") {
-            } else if ([1, 2, 3, 4, 5, 6, 7, 8].includes(action)) {
-            } else {
-                console.log("unknown action:", action);
             }
         });
 
         ws.on("close", () => {
-            Game.exit(ws.playerName);
-            wsServer.clients.forEach((client) => {
-                client.send(JSON.stringify({
-                    state: "exit",
-                    actionPlayer: ws.playerName,
-                    playerNames: Game.playerNames,
-                }));
-            });
+            Game.gameover(ws.playerName);
+            delete Game.playerState[ws.playerName];
+            update();
             console.log(ws.playerName, "closed connection");
         });
     });
